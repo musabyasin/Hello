8f816715ddd291a1e571c1a22dd654d30f218111
Musab yasin <musabyasin@gmail.com>
2024-11-28 07:07:07 -0500

    Hello

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.function.Consumer;
 import java.util.regex.*;
 
 public class Main {
@@ -15,6 +16,9 @@ public class Main {
         System.out.print("\n5: ");  //no space ?
         Pattern.compile("\\s+").splitAsStream("Hello Java").forEach(System.out::print);
         System.out.print("\n6: ");
-        new Thread(()->System.out.println("Hello Java")).start();
+        Consumer<String> w=(System.out::print);
+        w.accept("Hello Java");
+        System.out.print("\n7: ");
+        new Thread(()->System.out.print("Hello Java")).start();
     }
 }
